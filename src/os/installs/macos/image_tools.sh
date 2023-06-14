7d735e9b4d48477fbff363869422fafe759933df
you@example.com <you@example.com>
2023-06-14 05:12:37 -0400

    initial

---
+++ src/os/installs/macos/image_tools.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../../utils.sh" \
+    && . "./utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_in_purple "\n   Image Tools\n\n"
+
+brew_install "GIMP" "gimp" "--cask"
+brew_install "ImageMagick" "imagemagick"
+brew_install "ImageOptim" "imageoptim" "--cask"
+brew_install "Pngyu" "pngyu" "--cask"
