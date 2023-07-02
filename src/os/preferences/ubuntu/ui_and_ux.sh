727cdebd2c6b2691fe7a4a260e342a40436a966c
sotiriskast <soteris100@gmail.com>
2023-07-02 23:48:18 +0300

    update ubuntu files

--- src/os/preferences/ubuntu/ui_and_ux.sh
+++ src/os/preferences/ubuntu/ui_and_ux.sh
@@ -7,17 +7,11 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n   UI & UX\n\n"
 
-#execute "gsettings set org.gnome.desktop.background picture-options 'stretched'" \
-#    "Set desktop background image options"
-
-#execute "gsettings set org.gnome.libgnomekbd.keyboard layouts \"[ 'us', 'gr' ]\"" \
-#    "Set keyboard languages"
-
-#execute "gsettings set com.canonical.Unity.Launcher favorites \"[
-#            'ubiquity-gtkui.desktop',
-#            'nautilus-home.desktop'
-#         ]\"" \
-#    "Set Launcher favorites"
-
+execute "gsettings --schemadir ~/.local/share/gnome-shell/extensions/caffeine@patapon.info/schemas/ get org.gnome.shell.extensions.caffeine toggle-state" \
+    "get caffeine"
+execute "gsettings --schemadir ~/.local/share/gnome-shell/extensions/caffeine@patapon.info/schemas/ set org.gnome.shell.extensions.caffeine toggle-state true" \
+    "Enable Caffeine"
+execute "gsettings set org.gnome.desktop.background picture-options 'stretched'" \
+    "Set desktop background image options"
 execute "gsettings set com.canonical.Unity.Lenses remote-content-search 'none'" \
     "Turn off 'Remote Search' so that search terms in Dash do not get sent over the internet"
