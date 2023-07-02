db4f786c117f5a7fea18371e93979b529e4db41b
sotiriskast <soteris100@gmail.com>
2023-07-02 23:36:32 +0300

    update ubuntu docker

--- src/os/installs/ubuntu/docker.sh
+++ src/os/installs/ubuntu/docker.sh
@@ -30,10 +30,11 @@ installDocker() {
         "Verify key"
 
     execute \
-        "sudo add-apt-repository \
-        "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-         "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-        sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        "sudo add-apt-repository" \
+        "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu" \
+        ""$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+        "sudo tee /etc/apt/sources.list.d/docker.list > /dev/null"
+
 
     update
 
--- src/os/preferences/ubuntu/keybindings.sh
+++ src/os/preferences/ubuntu/keybindings.sh
@@ -16,14 +16,14 @@ execute "gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-left \"[
 execute "gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-right \"['<Primary><Alt><Super>Right']\"" \
     "move-to-monitor-right <Primary><Alt><Super>Right"
 
-execute "gsettings set org.gnome.settings-daemon.plugins.media-keys area-screenshot-clip \"['<Primary><Shift><Alt>dollar']\"" \
-    "area-screenshot-clip <Primary><Shift><Alt>dollar"
-
-execute "gsettings set org.gnome.settings-daemon.plugins.media-keys window-screenshot-clip \"['<Primary><Shift><Alt>numbersign']\"" \
-    "window-screenshot-clip <Primary><Shift><Alt>numbersign"
-
-execute "gsettings set org.gnome.settings-daemon.plugins.media-keys area-screenshot \"['<Shift><Alt>dollar']\"" \
-    "area-screenshot <Shift><Alt>dollar"
-
-execute "gsettings set org.gnome.settings-daemon.plugins.media-keys window-screenshot \"['<Shift><Alt>numbersign']\"" \
-    "window-screenshot <Shift><Alt>numbersign"
+#execute "gsettings set org.gnome.settings-daemon.plugins.media-keys area-screenshot-clip \"['<Primary><Shift><Alt>dollar']\"" \
+#    "area-screenshot-clip <Primary><Shift><Alt>dollar"
+#
+#execute "gsettings set org.gnome.settings-daemon.plugins.media-keys window-screenshot-clip \"['<Primary><Shift><Alt>numbersign']\"" \
+#    "window-screenshot-clip <Primary><Shift><Alt>numbersign"
+#
+#execute "gsettings set org.gnome.settings-daemon.plugins.media-keys area-screenshot \"['<Shift><Alt>dollar']\"" \
+#    "area-screenshot <Shift><Alt>dollar"
+#
+#execute "gsettings set org.gnome.settings-daemon.plugins.media-keys window-screenshot \"['<Shift><Alt>numbersign']\"" \
+#    "window-screenshot <Shift><Alt>numbersign"
