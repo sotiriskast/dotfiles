3799280cd37f36b3ca2e823a18820dd04e0b6f51
sotiris <soteris100@gmail.com>
2024-02-05 10:00:05 +0200

    update flutter package and install by snap

--- src/os/installs/ubuntu/flutter.sh
+++ src/os/installs/ubuntu/flutter.sh
@@ -13,10 +13,11 @@ declare -r FLUTTER_DIRECTORY="$HOME/Code/flutter"
 declare -r FLUTTER_BIN_DIRECTORY="$FLUTTER_DIRECTORY/bin"
 
 download_flutter() {
-    execute "git clone https://github.com/flutter/flutter.git $FLUTTER_DIRECTORY -b stable --depth 1" \
-        "Download"
-    execute "$FLUTTER_BIN_DIRECTORY/flutter precache" \
-        "Pre-download development binaries"
+    install_package_via_snap "flutter" "--classic"
+#    execute "git clone https://github.com/flutter/flutter.git $FLUTTER_DIRECTORY -b stable --depth 1" \
+#        "Download"
+#    execute "$FLUTTER_BIN_DIRECTORY/flutter precache" \
+#        "Pre-download development binaries"
 }
 
 add_to_path() {
@@ -38,7 +39,7 @@ add_to_path() {
 main() {
     if [ ! -d "$FLUTTER_DIRECTORY" ]; then
         download_flutter
-        add_to_path
+#        add_to_path
     fi
 }
 
